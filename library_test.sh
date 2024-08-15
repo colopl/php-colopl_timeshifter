f7fb684a99253e2f904bd7396f7ab54b4ab88dd8
Go Kudo <zeriyoshi@gmail.com>
2024-08-15 16:24:37 +0900

    disable opcache

--- library_test.sh
+++ library_test.sh
@@ -9,10 +9,6 @@ cd "/project"
     make install
     docker-php-ext-enable "colopl_timeshifter"
   cd -
-  docker-php-ext-install "opcache"
-  echo "opcache.enable_cli=1"        >> "$(php-config --ini-dir)/opcache.ini"
-  echo "opcache.jit=tracing"         >> "$(php-config --ini-dir)/opcache.ini"
-  echo "opcache.jit_buffer_size=64M" >  "$(php-config --ini-dir)/opcache.ini"
   composer install
   composer exec -- phpunit "tests"
   composer exec -- phpstan
