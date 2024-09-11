6376a1bc704118a9b7f9f6818ba206d7d13ed723
Go Kudo <zeriyoshi@gmail.com>
2024-09-11 03:12:49 +0000

    fix format

--- ext/hook.c
+++ ext/hook.c
@@ -1,21 +1,20 @@
 /*
   +----------------------------------------------------------------------+
-  | COLOPL PHP TimeShifter.											  |
+  | COLOPL PHP TimeShifter.                                              |
   +----------------------------------------------------------------------+
-  | Copyright (c) COLOPL, Inc.										   |
+  | Copyright (c) COLOPL, Inc.                                           |
   +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,	  |
-  | that is bundled with this package in the file LICENSE, and is		|
-  | available through the world-wide-web at the following url:		   |
-  | http://www.php.net/license/3_01.txt								  |
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
   | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to		  |
-  | info@colopl.co.jp so we can mail you a copy immediately.			 |
+  | obtain it through the world-wide-web, please send a note to          |
+  | info@colopl.co.jp so we can mail you a copy immediately.             |
   +----------------------------------------------------------------------+
-  | Author: Go Kudo <g-kudo@colopl.co.jp>								|
+  | Author: Go Kudo <g-kudo@colopl.co.jp>                                |
   +----------------------------------------------------------------------+
 */
-
 #include "hook.h"
 
 #include "php.h"
