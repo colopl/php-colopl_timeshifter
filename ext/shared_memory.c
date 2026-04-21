0c2d5f08d55b2df45ed25537fcbce9ba0ad9af6b
Go Kudo <g-kudo@colopl.co.jp>
2026-04-21 08:08:33 +0000

    indent

--- ext/shared_memory.c
+++ ext/shared_memory.c
@@ -4,8 +4,8 @@
   +----------------------------------------------------------------------+
   | Copyright (c) COLOPL, Inc.                                           |
   +----------------------------------------------------------------------+
-    | This source file is subject to the BSD-3-Clause license that is      |
-    | bundled with this package in the file LICENSE.                       |
+  | This source file is subject to the BSD-3-Clause license that is      |
+  | bundled with this package in the file LICENSE.                       |
   +----------------------------------------------------------------------+
   | Author: Go Kudo <g-kudo@colopl.co.jp>                                |
   +----------------------------------------------------------------------+
@@ -19,7 +19,7 @@ bool sm_init(sm_t *sm, size_t size) {
 
     if ((sm->data = mmap(NULL, sm->size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0)) == MAP_FAILED) {
         return false;
-    } 
+    }
 
     if (sem_init(&sm->semaphore, 1, 1) != 0) {
         return false;
