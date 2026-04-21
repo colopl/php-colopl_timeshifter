9fa0ea6f6d0a91e400cf741fa0500c40bc6d2c38
Go Kudo <g-kudo@colopl.co.jp>
2026-04-21 07:50:42 +0000

    overhawl

--- ext/shared_memory.h
+++ ext/shared_memory.h
@@ -4,13 +4,8 @@
   +----------------------------------------------------------------------+
   | Copyright (c) COLOPL, Inc.                                           |
   +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | info@colopl.co.jp so we can mail you a copy immediately.             |
+  | This source file is subject to the BSD-3-Clause license that is      |
+  | bundled with this package in the file LICENSE.                       |
   +----------------------------------------------------------------------+
   | Author: Go Kudo <g-kudo@colopl.co.jp>                                |
   +----------------------------------------------------------------------+
